1bc29b5b5a4aeda01f42069134fe932fdd20516d
Preeti <singhp1992@gmail.com>
2019-07-17 09:59:50 +0200

    adding producer instead of contributor

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
 const movie = [
     {
-        title: 'Hop on Pop',
-        description: "A delightful children's book.",
-        type: 'book',
-        contributor: 'Dr. Suess',
+        title: 'The Lion King',
+        description: "A delightful children's movie.",
+        type: 'Family',
+        producer: 'Dr. Suess',
         showDetail: false,
     },
     {
         title: 'The Joy of Painting',
         description: "Create a world of happy little trees!",
         type: 'DVD',
-        contributor: 'Bob Ross',
+        producer: 'Bob Ross',
         showDetail: false
     },
     {
         title: 'Supernatural: The Complete 12th Season',
         description: "A (literally) neverending roadtrip.",
         type: 'DVD',
-        contributor: "",
+        producer: "",
         showDetail: false
     },
     {
         title: 'Planet Earth II',
         description: "Hours of beautiful but heart attack-inducing nature footage",
         type: 'streaming video',
-        contributor: 'David Attenborough',
+        producer: 'David Attenborough',
         showDetail: false,
     },
     {
         title: 'Titanic',
         description: "The boat sinks.",
         type: 'DVD',
-        contributor: 'James Cameron',
+        producer: 'James Cameron',
         showDetail: false,
     },
     {
         title: 'The Sirens of Titan',
         description: "Mankind flung its advance agents ever outward, ever outward... it flung them like stones.",
         type: 'book',
-        contributor: 'Kurt Vonnegut',
+        producer: 'Kurt Vonnegut',
         showDetail: false,
     },
     {
         title: 'Better Call Saul',
         description: "A slow-burning Breaking Bad prequel.",
         type: 'streaming video',
-        contributor: '',
+        producer: '',
         showDetail: false,
     },
     {
         title: 'Title 1',
         description: "A slow-burning Breaking Bad prequel.",
         type: 'e-book',
-        contributor: '',
+        producer: '',
         showDetail: false,
     },
     {
         title: 'Title 2',
         description: "A slow-burning Breaking Bad prequel.",
         type: 'e-book',
-        contributor: '',
+        producer: '',
         showDetail: false,
     }
 ]
